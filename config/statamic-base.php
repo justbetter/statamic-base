1736915a7c85f454dfbb6f0cb2d02ea387870681
Kevin Meijer <kevin@justbetter.nl>
2026-05-19 15:29:03 +0200

    Set icon urls

--- config/statamic-base.php
+++ config/statamic-base.php
@@ -8,7 +8,7 @@ return [
 
     'packagist_cache_ttl' => (int) env('STATAMIC_BASE_PACKAGIST_CACHE_TTL', 3600),
 
-    'icon_url' => env('STATAMIC_BASE_ICON_URL', 'https://opensource.justbetter.nl/statamic/justbetter-logo-small-black.svg'),
+    'icon_url' => 'https://opensource.justbetter.nl/statamic/justbetter-logo-small-black.svg',
 
-    'icon_dark_url' => env('STATAMIC_BASE_ICON_DARK_URL', 'https://opensource.justbetter.nl/statamic/justbetter-logo-small-white.svg'),
+    'icon_dark_url' => 'https://opensource.justbetter.nl/statamic/justbetter-logo-small-white.svg',
 ];
